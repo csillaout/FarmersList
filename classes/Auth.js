a9094116ca323d18b4ae476186b5c124c93fd689
Csilla Toth <csillaout@gmail.com>
2024-10-21 16:02:59 +0100

    auth

--- classes/Auth.js
+++ classes/Auth.js
@@ -1,1 +1,13 @@
-class Auth {}
+class Auth {
+  constructor() {
+    this.customer = [];
+  }
+  regitser(name, email, shippingAddress) {
+    this.customer.push(name, email, shippingAddress);
+  }
+  login(email) {
+    return customer[email];
+  }
+}
+
+module.exports = Auth;
